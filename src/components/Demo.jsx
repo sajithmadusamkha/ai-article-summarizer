4d51ff5210bcf0beb931a00b42abe11f4069d22f
sajithmadusamkha <sajithmadusankha02@gmail.com>
2024-04-12 22:58:24 +0530

    demo component updated

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const handleSubmit = async (e) => {
+    alert('submitted');
+  };
+
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
         <form
           className="relative flex justify-center items-center"
-          onSubmit={() => {}}
+          onSubmit={handleSubmit}
         >
           <img
             src={linkIcon}
@@ -18,8 +27,8 @@ const Demo = () => {
           <input
             type="url"
             placeholder="Paste the article link"
-            value=""
-            onChange={() => {}}
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
             required
             className="url_input peer"
           />
