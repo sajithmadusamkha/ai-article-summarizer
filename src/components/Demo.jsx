0a45a8d855804760debdbbdbded519f15be7145c
sajithmadusamkha <sajithmadusankha02@gmail.com>
2024-04-12 20:57:36 +0530

    demo component updated

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
   return (
-    <section className='mt-16 w-full max-w-xl'>
-      <div className='flex flex-col w-full gap-2'></div>
-    </section>
-  )
-}
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={() => {}}
+        >
+          <img
+            src={linkIcon}
+            alt="link-icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
 
-export default Demo
+          <input
+            type="url"
+            placeholder="Paste the article link"
+            value=""
+            onChange={() => {}}
+            required
+            className="url_input peer"
+          />
+
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 "
+          >
+            <p>↵</p>
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Demo;
